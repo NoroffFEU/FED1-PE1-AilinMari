aab0482675d3204a495aa220fd921d9142d018c3
AilinMari <ailin_scilla@hotmail.com>
2025-02-20 11:51:04 +0100

    ../

---
+++ js/Delete-edit.js
@@ -0,0 +1,53 @@
+async function deleteBlogpost(postId) {
+  const url = `https://v2.api.noroff.dev/blog/posts/ailin_user/${postId}`;
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) {
+    console.error("Please log in.");
+    return;
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete blog post");
+    }
+
+    console.log("Blog post deleted:", postId);
+
+    // Fetch the updated list of blog posts
+    await getBlogpost();
+  } catch (error) {
+    console.error("Error deleting blog post", error);
+  }
+}
+
+function createEditButton(blogpost) {
+  const editPostButton = document.createElement("button");
+  editPostButton.textContent = "Edit post";
+  editPostButton.className = "edit-btn";
+  editPostButton.addEventListener("click", () => {
+    window.location.href = `/post/edit.html?id=${blogpost.id}`;
+  });
+
+  return editPostButton;
+}
+
+function createDeleteButton(blogpost) {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete post";
+  deleteButton.className = "delete-btn";
+  deleteButton.setAttribute("data-id", blogpost.id);
+  deleteButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const postId = event.target.getAttribute("data-id");
+    await deleteBlogpost(postId);
+  });
+
+  return deleteButton;
+}

ff1f280c3a6b857d43880477057927137856e116
AilinMari <ailin_scilla@hotmail.com>
2025-02-20 13:22:03 +0100

    let blogposts = data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ async function getBlogpost() {
   try {
     const response = await fetch(sprinkledBliss);
     const { data } = await response.json();
-    blogposts = data;
+    let blogposts = data;
 
     renderBlogposts(blogposts);
     updateCarousel(blogposts);

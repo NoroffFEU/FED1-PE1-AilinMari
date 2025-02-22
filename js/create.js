1ef1986ea7577e16c5d5b46ce8b2a85111628eac
AilinMari <ailin_scilla@hotmail.com>
2025-02-22 18:58:52 +0100

    create.js

--- js/create.js
+++ js/create.js
@@ -1,62 +1,5 @@
-const sprinkledBliss = "https://v2.api.noroff.dev/blog/posts";
-let blogposts = [];
-let login = localStorage.getItem("userId");
-
-// async function getBlogpost() {
-//   try {
-//     const response = await fetch(`${sprinkledBliss}/ailin_user`);
-//     const { data } = await response.json();
-//     blogposts = data;
-
-//     createBlogpost(blogposts);
-//   } catch (error) {
-//     console.error("Error fetching blogposts", error);
-//   }
-
-//   // console.log("Blogposts:", blogposts);
-// }
-
-async function createBlogpost(title, content, imageUrl, imageAlt) {
-  console.log("Creating blog post...");
-  const url = "https://v2.api.noroff.dev/blog/posts/ailin_user";
-  const accessToken = localStorage.getItem("accessToken");
-  if (!accessToken) {
-    
-    // console.error("Please log in.");
-    return;
-  }
-  const data = {
-    title: title,
-    body: content,
-    media: {
-      url: imageUrl,
-      alt: imageAlt || "Default image description",
-    },
-    // author: data.author.id,
-  };
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to create blog post");
-    }
-
-    const result = await response.json();
-    // console.log("Blog post created:", result);
-
-    // Fetch the updated list of blog posts
-    await getBlogpost();
-  } catch (error) {
-    // console.error("Error creating blog post", error);
-  }
-}
+import { BlogApi } from "./api-client.js";
+let blogApi = new BlogApi();
 
 document.addEventListener("DOMContentLoaded", () => {
   // const result = localStorage.getItem("result");
@@ -72,8 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const content = document.getElementById("content").value;
       const imageUrl = document.getElementById("imageUrl").value;
       const imageAlt = document.getElementById("imageAlt").value;
+      
+      await blogApi.createBlogpost(title, content, imageUrl, imageAlt);
 
-      await createBlogpost(title, content, imageUrl, imageAlt);
       window.location.href = `../index.html`;
     });
   } else {
